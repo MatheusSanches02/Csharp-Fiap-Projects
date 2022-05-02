a48c5dd274afbdd645ef9a86e21df35325622916
MatheusSanches02 <rm87426@fiap.com.br>
2022-05-01 23:46:08 -0300

    Introdução a plataforma .net

---
+++ HelloWorld/HelloWorld/Model/HelloModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorld.Model
+{
+    public class HelloModel
+    {
+        public string Mensagem = "Hello FIap";
+    }
+}
---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿using HelloWorld.Model;
+using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloModel model = new HelloModel(); //f9 - breakpoint 
+                                                 //f10 - avança a linha
+            Console.WriteLine(model.Mensagem);
+
+            Console.Read();
+        }
+    }
+}
